a15a0b77d815444bf92efadb76a218eddbf7a01c
AustinMcCowan <amccowan@stcharlessd.org>
2021-04-13 12:53:00 -0400

    Started adding javascript for page changing

---
+++ js/site.js
@@ -0,0 +1,9 @@
+```
+    Austin McCowan
+    04/13/2021
+    js file used for handling pieces of javascript here and there
+```
+
+function pagechange(button) {
+    document.getElementById("iframe-id").src = button.value;
+}
