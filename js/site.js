e4f37812f81fb9949ab42d48671bcc47288d2d1f
AustinMcCowan <amccowan@stcharlessd.org>
2021-04-13 13:37:29 -0400

    Finished setting up buttons for page swapping

--- js/site.js
+++ js/site.js
@@ -1,9 +1,15 @@
-```
+/*
     Austin McCowan
     04/13/2021
     js file used for handling pieces of javascript here and there
-```
+*/
 
 function pagechange(button) {
+    // Change the page to the selected one
     document.getElementById("iframe-id").src = button.value;
+    // Revert current page button css to normal
+    document.getElementsByClassName("btn-currentpage")[0].className = "btn-page";
+    
+    // Change the css of the nav button that was just clicked to indicate what page the user is currently on
+    button.className = "btn-currentpage";
 }
