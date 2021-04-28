9726d16c6e59b001df714a79f6b236e1c0f7720f
AustinMcCowan <amccowan@stcharlessd.org>
2021-04-28 13:43:25 -0400

    Progressed index.html

--- js/site.js
+++ js/site.js
@@ -12,3 +12,27 @@ function pagechange(button) {
     // Change the css of the nav button that was just clicked to indicate what page the user is currently on
     button.className = "btn-currentpage";
 }
+
+
+// Set automatic offset for nav header based on div header height (IF KEEPING BOTH HEADERS)
+`
+try{
+    function resize() {
+        if (window.getComputedStyle(document.getElementById("div-header")).getPropertyValue('position') != "relative") {
+            let offset = document.getElementById("div-header").offsetHeight;
+            offset = String(offset) + "px";
+            document.getElementById("nav-header").style.top = offset;
+        }
+
+        else {
+            document.getElementById("nav-header").style.top = "0px";
+        }
+    }
+    resize();
+
+    window.addEventListener('resize', resize);
+}
+catch {
+    console.error("positioning failed");
+}
+`
